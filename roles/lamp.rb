f85b0d8f8521de4bbd871837a83a77d34fc601fb
James Walker <walkah@walkah.net>
2011-09-04 17:20:13 -0400

    always use apc and apt (just in case)

--- roles/lamp.rb
+++ roles/lamp.rb
@@ -1,9 +1,11 @@
 name "lamp"
 description "Basic LAMP stack"
 run_list(
+  "recipe[apt]",
   "recipe[mysql::server]",
   "recipe[php::package]",
   "recipe[php::module_mysql]",
+  "recipe[php::module_apc]",
   "recipe[apache2]",
   "recipe[apache2::mod_expires]",
   "recipe[apache2::mod_php5]",
