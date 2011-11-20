375896edf46df68bfb246b363a7b010c4789ce84
James Walker <walkah@walkah.net>
2011-11-20 13:40:35 -0500

    setup xdebug properly for remote debugging

--- site-cookbooks/drupal/recipes/dev.rb
+++ site-cookbooks/drupal/recipes/dev.rb
@@ -8,3 +8,10 @@ php_pear "xdebug" do
   action :install
 end
 
+template "/etc/php5/conf.d/xdebug.ini" do
+  source "xdebug.ini.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, resources("service[apache2]"), :delayed
+end
